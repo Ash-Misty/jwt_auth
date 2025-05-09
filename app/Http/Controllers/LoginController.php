c53167a1eb4ee524a8618f7b5cd8fe3590b1f773
Ash-Misty <charlinashini@gmail.com>
2025-05-09 10:49:38 +0530

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -44,17 +44,17 @@ class LoginController extends Controller
             'exp' => time() + 60 * 60 // 1 hour expiration
         ];
 
-        // For debugging: Dump the payload and exit (you will see it in the browser or logs)
-      //  dd($payload);  // This will stop execution and dump the payload for debugging
+       
+     
 
         // Encode the token
         $jwt = JWT::encode($payload, env('JWT_SECRET'), 'HS256');
 
         // Redirect with cookie
         return redirect('/home')->cookie(
-            'jwt_token', $jwt, 60,     // name, value, minutes
-            null, null,                // path, domain
-            false, true                // secure, httpOnly (set secure to true in production)
+            'jwt_token', $jwt, 60, 
+            null, null,          
+            false, true                
         );
     }
 
